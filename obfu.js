f018b2db74a38b70388b8bddd7ddc42f456ce235
NotAdl22 <georgangsaiod@gmail.com>
2024-12-05 11:56:24 +0100

    Update obfu.js

--- obfu.js
+++ obfu.js
@@ -9,7 +9,7 @@ function printCode() {
     }
 
     const set = "a" + Math.random().toString(36).substring(10); 
-    const letters = Array.from("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").sort(() => Math.random() - 0.5).join('');
+    const letters = Array.from("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ一二三四五六七八九十百千万亿天地人物他她它我你你们他人朋友家学校工作学习做吃喝说听走跑跳看想学玩玩具电话电电脑网络电视电影音乐书字词句阅读写写字画唱跳舞节目电视台报纸杂志新闻新闻播报新闻记者报道阅读器编辑制作演员导演音乐家艺术家作家演讲会聚会聚集分开休息旅游旅行国家城市街道公园商店超市餐厅咖啡馆商场书店电影院医院学院学术研究知识科学数学物理化学历史地理生物艺术体育体育馆足球篮球排球乒乓球羽毛球运动运动员比赛胜利失败奖杯奖状游戏卡片玩具电子产品手机照相机相机摄像机电视机空调电脑电子书电影演出音乐图书馆卫生健康疾病药物医生医院急救救护车手术治疗饮食食物饮料水茶咖啡果汁牛奶早餐午餐晚餐小吃零食甜点面包米饭面条菜饺子包子鱼肉鸡蔬菜水果苹果香蕉橙子桃子葡萄西瓜草莓樱").sort(() => Math.random() - 0.5).join('');
     const setlettre = "Set " + set + "=" + letters;
     let codeobfu = "";
     const lettertab = {};
